d8cc4e0e53ddfff8a029656a0c221dba1d03a9b2
Mariana Machohan <debrisofme9@gmail.com>
2022-10-11 21:50:00 +0200

    move img src

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 
-const Modal = ({open, setOpen }) => (
+const Modal = ({open, setOpen, children }) => (
 
 	<div className={`overlay animated ${open ? 'show' : ''}`}>
 		<div className="modal">
@@ -9,7 +9,9 @@ const Modal = ({open, setOpen }) => (
 				<title />
 				<path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
 			</svg>
-			<img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+			{/* Move to func with children */}
+			{/* <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />  */}
+			{children}
 		</div>
 	</div>
 
@@ -26,7 +28,9 @@ function App() {
 				{/* Without animation */}
 				{/* {open && <Modal open={open} setOpen={setOpen} />}  */}
 				{/* With animation */}
-				<Modal open={open} setOpen={setOpen} />	
+				<Modal open={open} setOpen={setOpen}>
+					<img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+				</Modal>
 			</div>
 		</>
   );
